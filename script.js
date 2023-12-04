e7014dd85497eb0e5df6f62cbb002df31c8c52c9
Batuhan Emiroğlu <74363208+batumento@users.noreply.github.com>
2023-12-04 16:39:45 +0300

    Practice HTTP Request and
    Theoretically, I read articles about how the Web works and I understood the logic of working in the background of HTTP Request by practising.

--- script.js
+++ script.js
@@ -4,8 +4,32 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-//Task yourself :Show the neighbors of the selected country and call up its data when we click on any of them. Note: try to write object oriented programming while doing this.
-const getCountryData = function (country) {
+//Task yourself :
+// 1. Show the neighbors of the selected country and call up its data when we click on any of them. Note: try to write object oriented programming while doing this.
+//2. Show the countries that officially use the languages we have chosen as mother tongues.
+
+const renderCountry = function (data) {
+  const languages = Object.values(data.languages)[0];
+  const currencies = Object.values(data.currencies)[0].name;
+  const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
@@ -13,26 +37,21 @@ const getCountryData = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    const languages = Object.values(data.languages)[0];
-    const currencies = Object.values(data.currencies)[0].name;
-    const html = `
-    <article class="country">
-            <img class="country__img" src="${data.flags.svg}" />
-            <div class="country__data">
-              <h3 class="country__name">${data.name.common}</h3>
-              <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(
-                +data.population / 1000000
-              ).toFixed(1)}</p>
-              <p class="country__row"><span>🗣️</span>${languages}</p>
-              <p class="country__row"><span>💰</span>${currencies}</p>
-            </div>
-          </article>
-    `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    const neighbours = data?.borders;
+    if (!neighbours) return;
+    neighbours.forEach(neighbour => {
+      const request2 = new XMLHttpRequest();
+      request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+      request2.send();
+
+      request2.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        renderCountry(data);
+      });
+    });
+
+    renderCountry(data);
   });
 };
 
-getCountryData('su');
-getCountryData('turkey');
+getCountryAndNeighbour('ESP');
