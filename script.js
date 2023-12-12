9512594733440eed196e008d46b71f66a29268df
Batuhan Emiroğlu <74363208+batumento@users.noreply.github.com>
2023-12-12 18:24:45 +0300

    Practice with Promises

--- script.js
+++ script.js
@@ -3,17 +3,16 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
-//Task yourself :
-// 1. Show the neighbors of the selected country and call up its data when we click on any of them. Note: try to write object oriented programming while doing this.
-//2. Show the countries that officially use the languages we have chosen as mother tongues.
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
 
-const renderCountry = function (data) {
+const renderCountry = function (data, className = '') {
   const languages = Object.values(data.languages)[0];
   const currencies = Object.values(data.currencies)[0].name;
   const html = `
-  <article class="country">
-          <img class="country__img" src="${data.flags.svg}" />
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}"/>
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
             <h4 class="country__region">${data.region}</h4>
@@ -26,9 +25,13 @@ const renderCountry = function (data) {
         </article>
   `;
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 };
+///////////////////////////////////////
+//Task yourself :
+// 1. Show the neighbors of the selected country and call up its data when we click on any of them. Note: try to write object oriented programming while doing this.
+//2. Show the countries that officially use the languages we have chosen as mother tongues.
 
+/*
 const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -36,22 +39,58 @@ const getCountryAndNeighbour = function (country) {
 
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    console.log(data);
+    renderCountry(data);
+
     const neighbours = data?.borders;
     if (!neighbours) return;
     neighbours.forEach(neighbour => {
       const request2 = new XMLHttpRequest();
       request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
       request2.send();
-
       request2.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        renderCountry(data);
+        const [data2] = JSON.parse(this.responseText);
+        renderCountry(data2, 'neighbour');
       });
     });
+  });
+};
+*/
+//getCountryAndNeighbour('Turkey');
 
-    renderCountry(data);
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
   });
 };
 
-getCountryAndNeighbour('ESP');
+const getCountryData = function (country) {
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, `Country not found`)
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbours = data[0].borders;
+
+      if (!neighbours) throw new Error('no neighbour found!');
+      neighbours.forEach(neighbour =>
+        getJSON(
+          `https://restcountries.com/v3.1/alpha/${neighbour}`,
+          `2: Country not found`
+        )
+          .then(dataN => {
+            renderCountry(dataN[0], 'neighbour');
+          })
+          .catch(err => alert(err + ' Bak'))
+      );
+    })
+    .catch(err => {
+      renderError(`Something went wrong ${err.message}♨♨♨`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('australia');
+});
