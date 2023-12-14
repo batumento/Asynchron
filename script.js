8006ab0843fd1136a70d62e2503d73154ef917d5
Batuhan Emiroğlu <74363208+batumento@users.noreply.github.com>
2023-12-14 14:54:52 +0300

    challenge#1

--- script.js
+++ script.js
@@ -92,5 +92,37 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('australia');
+  getCountryData('Germany');
 });
+///////////////////
+/* Coding Challenge #1*/
+
+const whereAmI = function (lat, lng) {
+  const coords = [lat, lng];
+  fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=949716826320982866157x93746`
+  )
+    .then(response => {
+      if (!response.ok) throw new Error('Rejected Promise Error 404');
+      return response.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.city}, ${data.country}`);
+      fetch(`https://restcountries.com/v3.1/name/${data.country}`)
+        .then(response => {
+          if (!response.ok)
+            throw new Error(`Rejected Promise: ${response.status}`);
+          return response.json();
+        })
+        .then(data => {
+          if (!data) throw new Error(`Data undefined or null`);
+          renderCountry(data[0]);
+        });
+    })
+    .catch(err => console.error(`You have a ${err}`))
+    .finally(() => (countriesContainer.style.opacity = 1));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
